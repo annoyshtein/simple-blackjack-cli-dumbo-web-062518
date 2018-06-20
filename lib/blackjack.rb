3eb96c0cf93d1c47bdee9d37ce1e0a629d92b027
Alex Neustein <alex.neustein@gmail.com>
2018-06-20 15:16:00 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -63,7 +63,7 @@ end
 #####################################################
 
 def runner
-  puts "running hit?"
+  puts "running runner"
   welcome
   card_total = initial_round
   until card_total >= 21
